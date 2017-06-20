e9933eeea8577e1fc08501e946949a46fe485681
Balaji Baduru <badurubalaji@gmail.com>
2017-06-20 09:09:05 -0700

    Add files via upload

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   username: string = '';
+  error_value='';
+  password='';
+  error='error';
 login(){
+  if(this.username.length<1)
+  {
+    
+  this.error_value='UserName must not be empty';
+  return;
+}
+if(this.password.length<1)
+  {
+  this.error_value='Password must not be empty';
+  return;
+}
+this.error_value='';
   console.log(this.username);
   
 }
